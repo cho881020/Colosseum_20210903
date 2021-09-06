87e24f3c870cf08d3d40d4f12998945afd81f169
조경진 <cho881020@gmail.com>
2021-09-06 15:36:50 +0900

    서버의 토픽목록에서 -> TopicData 목록으로 변환 작업중.
      - for문 사용
      - TopicData의 보조 생성자 추가

--- app/src/main/java/com/neppplus/colosseum_20210903/MainActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20210903/MainActivity.kt
@@ -2,10 +2,14 @@ package com.neppplus.colosseum_20210903
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.colosseum_20210903.datas.TopicData
 import com.neppplus.colosseum_20210903.utils.ServerUtil
 import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
+
+    val mTopicList = ArrayList<TopicData>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -29,6 +33,26 @@ class MainActivity : BaseActivity() {
 
         ServerUtil.getRequestMainData(mContext, object : ServerUtil.JsonResponseHandler {
             override fun onResponse(jsonObj: JSONObject) {
+
+//                응답 - jsonObj 분석 (파싱) => 토론 주제들을 서버가 내려줌.
+
+                val dataObj = jsonObj.getJSONObject("data")
+                val topicsArr = dataObj.getJSONArray("topics")
+
+//                서버가 내려주는 토론주제들 (JSONObject 목록) => TopicData로 변환해서 ArrayList에 추가. (반복문 활용)
+
+//                topicArr에 4개 : 0 ~ 4직전 까지 반복.
+
+                for (i  in   0 until  topicsArr.length()) {
+
+//                    순서에 맞는 { }를 통째로 받아내기. => 토론 주제 하나하나에 대한 정보를 담게됨.
+                    val topicObj = topicsArr.getJSONObject(i)
+
+//                    TopicData를 만들어서 -> 멤버변수들에 -> topicObj에서 파싱한 데이터를 대입.
+                    val tempTopicData = TopicData()
+
+
+                }
 
             }
 
--- app/src/main/java/com/neppplus/colosseum_20210903/datas/TopicData.kt
+++ app/src/main/java/com/neppplus/colosseum_20210903/datas/TopicData.kt
@@ -4,4 +4,11 @@ class TopicData(
     var id: Int,
     var title: String,
     var imageURL: String) {
+
+//    보조 생성자 추가.
+    constructor() : this(0, "제목없음", "")
+
+//    연습. id값만 받는 보조 생성자.
+    constructor(id: Int) : this(id, "제목없음", "")
+
 }
