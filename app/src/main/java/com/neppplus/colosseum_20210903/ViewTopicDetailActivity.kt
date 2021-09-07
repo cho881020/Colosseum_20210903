2b8da2f8e382831a1f259300446a91bbaa6e4953
조경진 <cho881020@gmail.com>
2021-09-07 11:08:43 +0900

    토론 상세정보 API 요청 -> 댓글 목록도 같이 불러옴.
      - mReplyList에 최종 추가

--- app/src/main/java/com/neppplus/colosseum_20210903/ViewTopicDetailActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20210903/ViewTopicDetailActivity.kt
@@ -3,6 +3,7 @@ package com.neppplus.colosseum_20210903
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.Glide
+import com.neppplus.colosseum_20210903.datas.ReplyData
 import com.neppplus.colosseum_20210903.datas.TopicData
 import com.neppplus.colosseum_20210903.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_view_topic_detail.*
@@ -11,6 +12,8 @@ import org.json.JSONObject
 class ViewTopicDetailActivity : BaseActivity() {
 
     lateinit var mTopicData : TopicData
+
+    val mReplyList = ArrayList<ReplyData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,6 +51,24 @@ class ViewTopicDetailActivity : BaseActivity() {
 //                mTopicData를 새로 파싱한 데이터로 교체.
 
                 mTopicData = TopicData.getTopicDataFromJson(topicObj)
+
+
+//                topicObj 안에를 보면, 댓글 목록도 같이 들어있다. => 추가 파싱, UI 반영
+
+                val repliesArr =  topicObj.getJSONArray("replies")
+
+                for (i  in  0 until repliesArr.length()) {
+
+//                    댓글 { } json -> ReplyData 파싱 (변환) -> mReplyList 목록에 추가.
+
+//                    val replyObj = repliesArr.getJSONObject(i)
+//                    val replyData = ReplyData.getReplyDataFromJson(replyObj)
+//                    mReplyList.add(replyData)
+
+                    mReplyList.add( ReplyData.getReplyDataFromJson( repliesArr.getJSONObject(i) ) )
+
+                }
+
 
 //                새로 받은 데이터로 UI 반영 (득표 수 등등)
                 refreshTopicDataToUI()
