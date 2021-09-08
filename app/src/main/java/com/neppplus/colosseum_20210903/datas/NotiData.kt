512c45f372ba5d3b0041d127790926a804b487a5
조경진 <cho881020@gmail.com>
2021-09-08 14:42:02 +0900

    알림 데이터 표현 클래스 추가  - json => NotiData로 바꿔주는 기능도 추가

---
+++ app/src/main/java/com/neppplus/colosseum_20210903/datas/NotiData.kt
@@ -0,0 +1,26 @@
+package com.neppplus.colosseum_20210903.datas
+
+import org.json.JSONObject
+
+class NotiData(
+    var id: Int,
+    var title: String) {
+
+    constructor() : this(0, "제목 없음")
+
+    companion object {
+
+        fun getNotiDataFromJson(json : JSONObject) : NotiData {
+
+            val notiData = NotiData()
+
+            notiData.id = json.getInt("id")
+            notiData.title = json.getString("title")
+
+            return notiData
+
+        }
+
+    }
+
+}
