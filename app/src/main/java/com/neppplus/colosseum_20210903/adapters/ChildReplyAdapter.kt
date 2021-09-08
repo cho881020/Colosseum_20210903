599356f36eff61ea82ae6b9596cd81ced5aca720
조경진 <cho881020@gmail.com>
2021-09-08 17:19:35 +0900

    답글 뿌려주는 어댑터 기초 작업

---
+++ app/src/main/java/com/neppplus/colosseum_20210903/adapters/ChildReplyAdapter.kt
@@ -0,0 +1,53 @@
+package com.neppplus.colosseum_20210903.adapters
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.neppplus.colosseum_20210903.R
+import com.neppplus.colosseum_20210903.ViewReplyDetailActivity
+import com.neppplus.colosseum_20210903.ViewTopicDetailActivity
+import com.neppplus.colosseum_20210903.datas.ReplyData
+import com.neppplus.colosseum_20210903.datas.TopicData
+import com.neppplus.colosseum_20210903.utils.ServerUtil
+import org.json.JSONObject
+import java.text.SimpleDateFormat
+
+class ChildReplyAdapter(
+    val mContext:Context,
+    resId: Int,
+    val mList: List<ReplyData>) : ArrayAdapter<ReplyData>(mContext, resId, mList) {
+
+    val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var row = convertView
+        if (row == null) {
+            row = mInflater.inflate(R.layout.child_reply_list_item, null)
+        }
+        row!!
+
+        val data = mList[position]
+
+
+        return row
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
