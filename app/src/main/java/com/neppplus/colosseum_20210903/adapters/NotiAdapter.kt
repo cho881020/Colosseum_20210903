99116d9766c25f39d313dfbdd8f25026ab037950
조경진 <cho881020@gmail.com>
2021-09-08 14:44:15 +0900

    어댑터 기본 작업 마무리

---
+++ app/src/main/java/com/neppplus/colosseum_20210903/adapters/NotiAdapter.kt
@@ -0,0 +1,52 @@
+package com.neppplus.colosseum_20210903.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.neppplus.colosseum_20210903.R
+import com.neppplus.colosseum_20210903.ViewTopicDetailActivity
+import com.neppplus.colosseum_20210903.datas.NotiData
+import com.neppplus.colosseum_20210903.datas.ReplyData
+import com.neppplus.colosseum_20210903.datas.TopicData
+import com.neppplus.colosseum_20210903.utils.ServerUtil
+import org.json.JSONObject
+import java.text.SimpleDateFormat
+
+class NotiAdapter(
+    val mContext:Context,
+    resId: Int,
+    val mList: List<NotiData>) : ArrayAdapter<NotiData>(mContext, resId, mList) {
+
+    val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var row = convertView
+        if (row == null) {
+            row = mInflater.inflate(R.layout.notification_list_item, null)
+        }
+        row!!
+
+        val data = mList[position]
+
+
+        return row
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
